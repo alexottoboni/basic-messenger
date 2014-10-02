a52861e44f4c120391f7af74fbe84574f0a7496d
Alex Ottoboni <alex@bulbasaur.local>
2014-10-02 15:00:53 -0700

    first commit

---
+++ echoclient.py
@@ -0,0 +1,24 @@
+# TCP Client Code
+ 
+host="127.0.0.1"            # Set the server address to variable host
+ 
+port=4446               # Sets the variable port to 4446
+ 
+from socket import *             # Imports socket module
+ 
+while 1:
+    s=socket(AF_INET, SOCK_STREAM)      # Creates a socket
+             # Connect to server address
+    s.connect((host,port))  
+
+
+    data = raw_input("Enter data: ")
+
+    s.send(data)
+         
+    msg=s.recv(1024)            # Receives data upto 1024 bytes and stores in variables msg
+         
+    print "Message from server : " + msg
+     
+    s.close()                            # Closes the socket
+    # End of code
---
+++ echoserver.py
@@ -0,0 +1,30 @@
+# TCP Server Code
+ 
+host="127.0.0.1"                # Set the server address to variable host
+port=4446                   # Sets the variable port to 4446
+from socket import *                # Imports socket module
+ 
+s=socket(AF_INET, SOCK_STREAM)
+ 
+s.bind((host,port))                 # Binds the socket. Note that the input to
+                                            # the bind function is a tuple
+ 
+                      # Sets socket to listening state with a  queue
+                                            # of 5 connection
+
+print "Listening for connections.. "
+
+s.listen(5)  
+
+while(1): 
+     
+    q,addr=s.accept()               # Accepts incoming request from client and returns
+                                                # socket and address to variables q and addr
+    data = q.recv(1024)
+                          
+    if data:                 
+        q.send(data)                        
+ 
+s.close()
+ 
+# End of code
